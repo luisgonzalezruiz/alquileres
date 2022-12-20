d77572712a528c519ce8ae1c6fe11122c7fb4af0
luisgonzalezruiz <luisgonzalezruiz1979@gmail.com>
2022-12-20 09:53:09 -0300

    probando pinia

--- resources/js/stores/counter.js
+++ resources/js/stores/counter.js
@@ -2,7 +2,11 @@ import { defineStore } from 'pinia'
 
 export const useCounterStore = defineStore('counter', {
   state: () => {
-    return { count: 0 }
+    return {
+        count: 0,
+        nombre:'',
+        edad:0
+    }
   },
   // could also be defined as
   // state: () => ({ count: 0 })
